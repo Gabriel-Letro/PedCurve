0cec5f0d39919bfff36a57dcad115f26d0b7df1a
Camila <camilatozati37@gmail.com>
2026-05-21 13:15:54 -0300

    chore: remove arquivos órfãos e atualiza .gitignore
    Remove scripts one-time (fetch_who.py, test.cjs, commit_github.bat),
    assets não utilizados (react.svg, vite.svg, hero.png), JSONs de
    z-scores redundantes e CSVs brutos da pasta source/. Atualiza
    .gitignore para excluir permanentemente essas categorias de arquivos.

--- test.cjs
+++
@@ -1,7 +0,0 @@
-const { Calculator, Patient } = require('who-growth');
-
-console.log('Calculator properties:', Object.keys(Calculator));
-console.log('Calculator prototype:', Object.getOwnPropertyNames(Calculator.prototype));
-
-const calc = new Calculator();
-console.log('calc properties:', Object.keys(calc));
